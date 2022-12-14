13cfbd03b448be8bd97a604166c697a9534361cd
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2022-12-14 22:30:06 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -3,6 +3,30 @@ using System.Diagnostics;
 
 namespace MisalignedSpace {
     class Misaligned {
+        
+        List<string> actualColorCodePairList = new List<string>();
+        List<string> colorCodePairList = new List<string>();
+        string[] actualColorCodePairArray = { };
+        string[] colorCodePairArray = { };
+        
+        static int getLongestMajorColorString(string[] majorColor)
+        {
+            string majorColorLongString = string.Empty;
+            for (int i = 1; i < majorColor.Length; i++)
+            {
+                if (majorColor[i].Length > majorColor[i-1].Length)
+                {
+                    majorColorLongString = majorColor[i];
+                }
+                else
+                {
+                    majorColorLongString = majorColor[i-1];
+                }
+                return majorColorLongString.Length;
+        }
+          
+            
+            
         static int printColorMap() {
             string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
