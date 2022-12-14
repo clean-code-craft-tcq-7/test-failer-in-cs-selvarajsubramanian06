efda9175b71799b95f0484b7600f1f864fce0549
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2022-12-14 22:13:57 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -1,21 +1,25 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
 namespace TshirtSpace {
     class Tshirt {
         static string Size(int cms) {
-            if(cms < 38) {
+            if(cms <= 38) 
+            {
                 return "S";
-            } else if(cms > 38 && cms < 42) {
+            } else if(cms > 38 && cms < 42) 
+            {
                 return "M";
-            } else {
+            } else 
+            {
                 return "L";
             }
         }
         static void Main(string[] args) {
+            Debug.Assert(Size(37) == "S");
+            Debug.Assert(Size(40) == "M");
+            Debug.Assert(Size(43) == "L");
             Debug.Assert(Size(38) == "S");
-            Debug.Assert(Size(42) == "M");
-            Debug.Assert(Size(41) == "L");
             Console.WriteLine( "All is well (maybe!)");
         }
     }
