b4a945a1c0ddb1efb69156a6b50b9edabcf0a432
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2022-12-14 22:36:28 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -25,11 +25,54 @@ namespace MisalignedSpace {
                 return majorColorLongString.Length;
         }
           
+        static int getLongestMinorColorString(string[] minorColor)
+        {
+            string minorColorLongString = string.Empty;
+            for (int i = 1; i < minorColor.Length; i++)
+            {
+                if (minorColor[i].Length < minorColor[i - 1].Length)
+                {
+                    minorColorLongString = minorColor[i];
+                }
+                else
+                {
+                    minorColorLongString = minorColor[i - 1];
+                }
+            }
+
+            return minorColorLongString.Length;
+        }   
             
+        static string addBlankSpace(int blankSpace, string line)
+        {
+            for (int i = 0; i < blankSpace; i++)
+            {
+                line = line + " ";
+            }
+            return line;
+        }
+
+        static string[] correctMisalignedString(string[] array, int longestArrayLength)
+        {
+            List<string> newArray = new List<string>();
+            foreach(string arr in array)
+            {
+                int lengthDifference = longestArrayLength - arr.Length;
+                newArray.Add(addBlankSpace(lengthDifference, arr));
+            }
+            return newArray.ToArray();
+        }            
             
         static int printColorMap() {
             string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
+            
+            int longestMajorColorString = getLongestMajorColorString(majorColors);
+            int longestMinorColorString = getLongestMinorColorString(minorColors);
+
+            majorColors = correctMisalignedString(majorColors, longestMajorColorString);
+            minorColors = correctMisalignedString(minorColors, longestMinorColorString);
+            
             int i = 0, j = 0;
             for(i = 0; i < 5; i++) {
                 for(j = 0; j < 5; j++) {
